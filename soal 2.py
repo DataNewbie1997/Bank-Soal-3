aae0b17d788087c05921870118eec03148917d96
DataNewbie1997 <80762711+DataNewbie1997@users.noreply.github.com>
2021-03-16 21:41:23 +0700

    Update soal 2.py

--- soal 2.py
+++ soal 2.py
@@ -1,4 +1,10 @@
-a = list(map(int, input("Masukkan nomor telepon: ").split()))
-print(len(a))
-if len(a) >= 13:
-    print('nomor HP hanya maksimal 13 angka')
+def nomorHP (x):
+    if len(x) >= 13:
+        print('Nomor HP maksimal 13 angka')
+    else:
+        if (x[0:2]) != '08':
+            print('Nomor HP harus dimulai dengan "08"')
+        else: 
+            print('Nomor HP saya adalah',x)
+x = input('masukkan nomor HP:')
+nomorHP(x)
