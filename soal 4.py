50de608e0d0ec75bcc2b6f8ad5de52e418d3b6fb
DataNewbie1997 <80762711+DataNewbie1997@users.noreply.github.com>
2021-03-17 18:49:59 +0700

    Update soal 4.py

--- soal 4.py
+++ soal 4.py
@@ -1,8 +1,9 @@
 for item in range(10,1,-1):
     print('')
     for item2 in range (1,item):
-        print(item2,end='')
-    for item3 in range(item,1,-1):
+        print(' ',end='')
+    for item3 in range(item,1,-2):
         print('',end='')
     for item4 in range(item3,1,-1):
-        print('*',end='')
+        item4 ='*'
+        print(item4,end=' ')
